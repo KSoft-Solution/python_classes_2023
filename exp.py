035909a40046033749f2d951c34a3ea777f39818
KSoft-Solution <kanhasahu955902@gmail.com>
2022-12-30 21:59:29 +0530

    modules

---
+++ exp.py
@@ -0,0 +1,25 @@
+# import modules_userDefined
+
+# print(modules_userDefined.add(1,2))
+# print(modules_userDefined.substract(10,50))
+# print(modules_userDefined.multiple(20,30))
+# print(modules_userDefined.divide(5,10))
+
+# from modules_userDefined import add,substract,multiple,divide
+
+# print(add(1,2))
+# print(substract(10,50))
+# print(multiple(20,30))
+# print(divide(5,10))
+
+# import modules_userDefined as mu
+
+# print(mu.add(1,2))
+# print(mu.substract(10,50))
+# print(mu.multiple(20,30))
+# print(mu.divide(5,10))
+
+# from modules_userDefined import  *
+
+# print(add(2,6))
+# print(multiple(7,8))
---
+++ modules_userDefined.py
@@ -0,0 +1,14 @@
+def add(a: int, b: int) -> int:
+    return a+b
+
+
+def substract(a: int, b: int) -> int:
+    return a-b
+
+
+def multiple(a: int, b: int) -> int:
+    return a*b
+
+
+def divide(a: int, b: int) -> int:
+    return a/b
